38844ab264e0c300674a193fbee646cd296c81cc
ivekhov <ivekhov@gmail.com>
2022-08-30 00:07:40 +0300

    Add attempts om stocks.

--- stocks/stocks.py
+++ stocks/stocks.py
@@ -19,41 +19,40 @@ def limit_gmv(df: pd.DataFrame) -> pd.DataFrame:
     """
     df_arr = np.array(df)
 
-    res = np.where(
-        df_arr[:, 1] > df_arr[:, 2] * df_arr[:, 3],
+    df_arr[:, 1] = np.where(
+        df_arr[:, 3] <= 0,
+        0,
+        df_arr[:, 1]
+    )
+
+    df_arr[:, 1] = np.where(
+        df_arr[:, 2] <= 0,
+        0,
+        df_arr[:, 1]
+    )
+
+    df_arr[:, 1] = np.where(
+        df_arr[:, 1] < 0,
+        0,
+        df_arr[:, 1]
+    )
+
+    df_arr[:, 1] = np.where(
+        np.floor(df_arr[:, 1] / df_arr[:, 2]) > df_arr[:, 3],
         df_arr[:, 2] * df_arr[:, 3],
         df_arr[:, 1]
     )
 
-    res = np.where(
-        res % df_arr[:, 2] != 0,
-        np.floor(
-            res / df_arr[:, 2]
-        ) * df_arr[:, 2],
-        res
+    df_arr[:, 1]  = np.where(
+        df_arr[:, 1] %  df_arr[:, 2] != 0,
+        np.floor(df_arr[:, 1] / df_arr[:, 2]) * df_arr[:, 2],
+        df_arr[:, 1]
     )
 
-    res = np.where(
-        df_arr[:, 3] <= 0,
-        0,
-        res
-    )
-
-    res = np.where(
-        df_arr[:, 2] <= 0,
-        0,
-        res
-    )
-
-    res = np.where(
-        res < 0,
-        0,
-        res
-    )
     return pd.DataFrame(
         {
             'sku': df_arr[:, 0],
-            'gmv': res,
+            'gmv': np.float64(df_arr[:, 1]),
             'price': np.float64(df_arr[:, 2]),
             'stock': df_arr[:, 3],
         }
