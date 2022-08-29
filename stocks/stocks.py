1013190f0b38e655d02439f96253f6fe84d0dd5b
ivekhov <ivekhov@gmail.com>
2022-08-29 23:19:50 +0300

    Add attempts om stocks.

--- stocks/stocks.py
+++ stocks/stocks.py
@@ -1,37 +1,5 @@
-import math
 import numpy as np
 import pandas as pd
-
-
-def vect_gmv(arr):
-    """
-    Calculates updated gmv.
-
-    Parameters
-    ----------
-    arr : numpy array
-        Array with varibales. Elements of array: [0] - sku, [1] - gmv, [2] - price, [3] - stock.
-
-    Returns
-    -------
-    int
-        Updated value of gmv
-
-    """
-    if arr[1] <= 0:
-        return 0
-    if arr[2] < 0:
-        return 0
-    if arr[3] <= 0:
-        return 0
-
-    if arr[1] > (arr[2] * arr[3]):
-        return arr[2] * arr[3]
-
-    if arr[1] % arr[2] != 0:
-        return math.floor(arr[1] / arr[2]) * arr[2]
-
-    return arr[1]
 
 
 def limit_gmv(df: pd.DataFrame) -> pd.DataFrame:
@@ -50,13 +18,43 @@ def limit_gmv(df: pd.DataFrame) -> pd.DataFrame:
 
     """
     df_arr = np.array(df)
-    gmv_upd = np.apply_along_axis(func1d=vect_gmv, axis=1, arr=df_arr)
 
+    res = np.where(
+        df_arr[:, 1] > df_arr[:, 2] * df_arr[:, 3],
+        df_arr[:, 2] * df_arr[:, 3],
+        df_arr[:, 1]
+    )
+
+    res = np.where(
+        res % df_arr[:, 2] != 0,
+        np.floor(
+            res / df_arr[:, 2]
+        ) * df_arr[:, 2],
+        res
+    )
+
+    res = np.where(
+        df_arr[:, 3] <= 0,
+        0,
+        res
+    )
+
+    res = np.where(
+        df_arr[:, 2] <= 0,
+        0,
+        res
+    )
+
+    res = np.where(
+        res < 0,
+        0,
+        res
+    )
     return pd.DataFrame(
         {
             'sku': df_arr[:, 0],
-            'gmv': gmv_upd,
-            'price': df_arr[:, 2],
+            'gmv': res,
+            'price': np.float64(df_arr[:, 2]),
             'stock': df_arr[:, 3],
         }
     )
